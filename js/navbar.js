60936221c910c1c876df1c2deab1921cb01556fd
josh <mentoce@gmail.com>
2016-12-12 18:07:48 -0800

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop(),
+        navbar = $('.navbar-default');
+
+    if (scrollPos > 20) {
+      navbar.addClass('change-color');
+    } else {
+      navbar.removeClass('change-color');
+    }
+  });
+});
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+});
